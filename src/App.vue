650925786e04965dc97e79786f92cd5ec6392486
kjj7999 <kjj7999@gmail.com>
2023-03-11 02:20:18 +0900

    tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <router-view />
 </template>
 
 <style>
