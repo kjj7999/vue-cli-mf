290740ac5aec86d91aed9b14ccaf453b6d8bf6ff
kjj7999 <kjj7999@gmail.com>
2023-03-12 12:41:04 +0900

    output dir

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.output.filename = 'container/js/[name].[contenthash:8].min.js'
+      config.output.chunkFilename = 'container/js/[name].[contenthash:8].min.js'
+      config.output.cssFilename = 'container/css/[name].[contenthash:8].css'
+    } else {
+      config.output.filename = 'container/js/[name].js'
+      config.output.chunkFilename = 'container/js/[name].js';
+    }
+  }
 });
