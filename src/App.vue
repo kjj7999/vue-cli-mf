64a9c9ed51ddf39d6c80a939a739d2413d070f8f
kjj7999 <kjj7999@gmail.com>
2023-03-12 12:42:07 +0900

    add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
+  <NavigationBar></NavigationBar>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <router-view />
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import NavigationBar from '@/components/NavigationBar.vue';
+
+@Options({
+  components: {
+    NavigationBar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style>
 #app {
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="flex flex-wrap items-center justify-between p-3 bg-teal-500">
+    <div class="flex items-center flex-shrink-0 mr-6 text-white">
+      <span class="nav-title"> Navigation Bar </span>
+    </div>
+    <div class="flex-grow block w-full lg:flex lg:w-auto lg:items-center">
+      <div class="text-sm lg:flex-grow">
+        <router-link to="/" class="nav-item"> Home </router-link>
+        <router-link to="/about" class="nav-item"> About </router-link>
+        <a class="nav-item"> Blog </a>
+      </div>
+      <div>
+        <a href="#" class="nav-btn"> Download</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class NavigationBar extends Vue {}
+</script>
+
+<style scoped>
+.nav-title {
+  @apply text-xl font-semibold tracking-tight;
+}
+
+.nav-item {
+  @apply mt-4 mr-4 block text-teal-200 hover:text-white lg:mt-0 lg:inline-block;
+}
+
+.nav-btn {
+  @apply mt-4 inline-block rounded border border-white px-5 py-2 text-sm leading-none text-white lg:mt-0;
+}
+
+.nav-btn:hover {
+  @apply border-transparent bg-white text-teal-500;
+}
+</style>
