561beaeeaed747b8a4fa21dbe7078c4a488979a6
kjj7999 <kjj7999@gmail.com>
2023-03-12 13:07:54 +0900

    css output dir

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,15 @@ module.exports = defineConfig({
     if (process.env.NODE_ENV === "production") {
       config.output.filename = 'container/js/[name].[contenthash:8].min.js'
       config.output.chunkFilename = 'container/js/[name].[contenthash:8].min.js'
-      config.output.cssFilename = 'container/css/[name].[contenthash:8].css'
     } else {
       config.output.filename = 'container/js/[name].js'
       config.output.chunkFilename = 'container/js/[name].js';
     }
-  }
+  },
+  css: {
+    extract: {
+      filename: 'container/css/[name].[contenthash:8].css',
+      chunkFilename: 'container/css/chunk-[name].[contenthash:8].css'
+    }
+  },
 });
